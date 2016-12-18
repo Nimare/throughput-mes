0210cabbd3f029306e6d0f0b2f1e61a08cb3082c
Milan Gyorki <gyorkimilan@gmail.com>
2016-12-18 01:04:44 +0100

    Open socket on server side

--- server/server_main.c
+++ server/server_main.c
@@ -1,7 +1,21 @@
+#include <sys/types.h>
+#include <sys/socket.h> 
+#include <stdio.h>
+#include <stdlib.h>
 
-
+void error(const char *msg)
+{
+  perror(msg);
+  exit -1;
+}
 
 int main(int argc, char *argv[])
 {
+  int socketfd;
   
+  socketfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (socketfd < 0)
+  {
+    error("ERROR opening socket ");
+  }
 }
